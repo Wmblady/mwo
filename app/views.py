d4a828d86eaaae3e9ce34b48dbe63c19462cf281
Mary842 <marioka4@gmail.com>
2017-10-30 23:16:21 +0100

    first grid

--- app/views.py
+++ app/views.py
@@ -13,6 +13,24 @@ def before_request():
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
-#@login_required
 def index():
-	return render_template('index.html')
+	#w pythonie False i True pisze sie z duzej litery, klucze w slowniku musza byc w ciapkach jesli sa zwyklymi stringami inaczej szuka zmiennej
+	#mockTables symuluja odpowiedz z bazy
+	mockTables = [{"id": 1, "consoleType": False, "xAxis": 3, "yAxis": 4, "seats": 5, "price": 10}, {"id": 2, "consoleType": True, "xAxis": 2, "yAxis": 2, "seats": 3, "price": 12}]
+	
+	#to co w srodku [] buduje tablice zlozona z wartosci yAxis z kazdego stolu, potem max zwraca z tego maksymalna wartosc
+	yRange = max([table["yAxis"] for table in mockTables])
+	xRange = max([table["xAxis"] for table in mockTables])
+	
+	#na poczatku tworze calego grida
+	#_ oznacza ze olewam iterator i nie przypisuje go do zadnej zmiennej
+	#range(k, n) oznacza stworzenie tablicy [k, ..., n - 1]
+	gridTables = [[{"isTable": False} for _ in range(0, xRange)] for _ in range(0, yRange)]
+	for table in mockTables:
+		#dodawanie nowego klucza w pythonie wyglada tak, ze wystarczy zrobic tab['nowy klucz']
+		gridTables[table['yAxis'] - 1][table['xAxis'] - 1]['table'] = table
+		gridTables[table['yAxis'] - 1][table['xAxis'] - 1]['isTable'] = True
+		
+	#wszystkie dane sa przekazywane w momencie renderowania templatki, jesli chcemy cos dociagnac to za pomoca flaska trzeba na nowo wyrenderowac strone z tego co wiem, na razie tak probujmy
+	#jesli trzeba bedzie zmienic to cos poszukam
+	return render_template('index.html', tables=gridTables)
